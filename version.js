dbe9ef5822249785a1bbb3d249048a5a04e40547
alen-x <alen-x@tutanota.com>
2022-10-06 19:59:34 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":10082,"VerName":"1.8.2","Url":"https://redeslab.github.io/simplevpn.apk","Des":["1.优化用户体验"]}
+{"Ver":10087,"VerName":"1.8.7","Url":"https://redeslab.github.io/simplevpn.apk","Des":["1.优化用户体验"]}
